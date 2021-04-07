bfa83a06822fed2fc6e1cf65bec0afbde0ccd43c
Bohdan Panashenko <61510364+FaustTheFirst@users.noreply.github.com>
2021-04-07 12:21:37 +0300

    add mediator between repository and routes, update routes

--- server/api/routes/hotDogRoutes.js
+++ server/api/routes/hotDogRoutes.js
@@ -1,12 +1,29 @@
 import { Router } from 'express';
+import {
+  addHotDog,
+  changeHotDog,
+  getAllHotDogs,
+  getHotDog,
+  removeHotDog
+} from '../services/hotDogService.js';
 
 const router = Router();
 
 router
-  .get('/', (req, res) => res.send('Get all hot dogs'))
-  .get('/:id', (req, res) => res.send(`Get specific hot dog with id: ${req.params.id}`))
-  .post('/', (req, res) => res.send(` Create hot dog with body: ${req.body}`))
-  .put('/:id', (req, res) => res.send(`Change hot dog with id: ${req.params.id}; body: ${req.body}`))
-  .delete('/:id', (req, res) => res.send(`Delete hot dog with id: ${req.params.id}`));
+  .get('/', (req, res, next) => getAllHotDogs()
+    .then(payload => res.status(200).send(payload))
+    .catch(next))
+  .get('/:id', (req, res, next) => getHotDog(req.params.id)
+    .then(payload => res.status(200).send(payload))
+    .catch(next))
+  .post('/', (req, res, next) => addHotDog(req.body)
+    .then(payload => res.status(200).send(payload))
+    .catch(next))
+  .put('/:id', (req, res, next) => changeHotDog(req.params.id, req.body)
+    .then(payload => res.status(200).send(payload))
+    .catch(next))
+  .delete('/:id', (req, res, next) => removeHotDog(req.params.id)
+    .then(() => res.status(200).send({ success: true }))
+    .catch(next));
 
 export default router;
