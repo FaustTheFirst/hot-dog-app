593fb16565b482cd0f82ab67aee602f7a6937c21
Bohdan Panashenko <61510364+FaustTheFirst@users.noreply.github.com>
2021-04-09 20:45:36 +0300

    add thunks that handle request

---
+++ client/src/thunks.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createOne, deleteOne, getAll, getOne, updateOne } from './requestHelper';
+
+export const getAllHotDogs = createAsyncThunk(
+  'hotdog/getAll',
+  (_, { rejectWithValue }) => getAll()
+    .then(res => res.data).catch(err => rejectWithValue(err.response.data))
+);
+
+export const getHotDog = createAsyncThunk(
+  'hotdog/get',
+  (id, { rejectWithValue }) => getOne(id)
+    .then(res => res.data).catch(err => rejectWithValue(err.response.data))
+);
+
+export const addHotDog = createAsyncThunk(
+  'hotdog/add',
+  (body, { rejectWithValue }) => createOne(body)
+    .then(res => res.data).catch(err => rejectWithValue(err.response.data))
+);
+
+export const editHotDog = createAsyncThunk(
+  'hotdog/edit',
+  (idAndBody, { rejectWithValue }) => updateOne(idAndBody)
+    .then(res => res.data).catch(err => rejectWithValue(err.response.data))
+);
+
+export const removeHotDog = createAsyncThunk(
+  'hotdog/remove',
+  (id, { rejectWithValue }) => deleteOne(id)
+    .then(res => res.data).catch(err => rejectWithValue(err.response.data))
+);
