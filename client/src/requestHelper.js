f163360d63cb9be5f25dda430e4678877e455911
Bohdan Panashenko <61510364+FaustTheFirst@users.noreply.github.com>
2021-04-09 20:44:46 +0300

    add request functions

--- client/src/requestHelper.js
+++ client/src/requestHelper.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
+axios.defaults.headers['Content-Type'] = 'application/json';
 
-const getTest = () => axios.get('/test')
-  .then(res => res)
-  .catch(err => err);
+export const getAll = () => axios.get('/hotdog/');
 
-export default getTest;
+export const getOne = id => axios.get(`/hotdog/${id}`);
+
+export const createOne = body => axios.post('/hotdog/', body);
+
+export const updateOne = ({ id, body }) => axios.put(`/hotdog/${id}`, body);
+
+export const deleteOne = id => axios.delete(`/hotdog/${id}`);
