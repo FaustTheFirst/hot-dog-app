72ae003bd77a0faabae2427836f4df495add2a0d
Bohdan Panashenko <61510364+FaustTheFirst@users.noreply.github.com>
2021-04-10 17:15:10 +0300

    add various messages

--- client/src/Components/Notification.jsx
+++ client/src/Components/Notification.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Message, TransitionablePortal } from 'semantic-ui-react';
-import { dissmissMessage, getStatus } from '../entity';
+import { dissmissMessage, getMessage, getStatus } from '../entity';
 
 const Notification = () => {
   const dispatch = useDispatch();
   const status = useSelector(getStatus());
+  const message = useSelector(getMessage());
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => setIsOpen(status === 'error' || status === 'success'), [status]);
 
   return (
     <TransitionablePortal
       transition={{ animation: 'fade left', duration: 500 }}
-      open={status === 'error' || status === 'success'}
+      open={isOpen}
       onClose={() => dispatch(dissmissMessage())}
     >
       <Message
         success={status === 'success'}
         error={status === 'error'}
-        content="Sample message"
+        header={status}
+        content={message}
+        onDismiss={() => setIsOpen(false)}
         style={{ bottom: '10%', position: 'fixed', right: '10%', zIndex: 1000 }}
       />
     </TransitionablePortal>
--- client/src/entity.js
+++ client/src/entity.js
@@ -7,7 +7,7 @@ const hotDogAdapter = createEntityAdapter({
 /* eslint-disable no-param-reassign */
 const hotDogSlice = createSlice({
   name: 'hotDog',
-  initialState: hotDogAdapter.getInitialState({ status: 'idle', modal: undefined }),
+  initialState: hotDogAdapter.getInitialState({ status: 'idle', message: null, modal: null }),
   reducers: {
     openCreateModal: (state, { payload }) => {
       state.modal = payload;
@@ -20,6 +20,7 @@ const hotDogSlice = createSlice({
     },
     dissmissMessage: state => {
       state.status = 'idle';
+      state.message = null;
     }
   },
   extraReducers: {
@@ -30,49 +31,58 @@ const hotDogSlice = createSlice({
       state.status = 'idle';
       hotDogAdapter.setAll(state, payload);
     },
-    [getAllHotDogs.rejected]: state => {
+    [getAllHotDogs.rejected]: (state, { payload }) => {
       state.status = 'error';
+      state.message = `Cannot load data: ${payload.type}`;
     },
     [getHotDog.pending]: state => {
       state.status = 'loadingOne';
     },
     [getHotDog.fulfilled]: state => {
       state.status = 'success';
+      state.message = 'Hot dog found';
     },
-    [getHotDog.rejected]: state => {
+    [getHotDog.rejected]: (state, { payload }) => {
       state.status = 'error';
+      state.message = `Cannot load hot dog: ${payload.type}`;
     },
     [addHotDog.pending]: state => {
       state.status = 'creating';
     },
     [addHotDog.fulfilled]: (state, { payload }) => {
       state.status = 'success';
+      state.message = 'Hot dog created successfully';
       hotDogAdapter.addOne(state, payload);
     },
-    [addHotDog.rejected]: state => {
+    [addHotDog.rejected]: (state, { payload }) => {
       state.status = 'error';
+      state.message = `Cannot create hot dog: ${payload.type}`;
     },
     [editHotDog.pending]: state => {
       state.status = 'updating';
     },
     [editHotDog.fulfilled]: (state, { payload }) => {
       state.status = 'success';
+      state.message = 'Hot dog updated successfully';
       const { id, ...changes } = payload;
       hotDogAdapter.updateOne(state, { id, changes });
     },
-    [editHotDog.rejected]: state => {
+    [editHotDog.rejected]: (state, { payload }) => {
       state.status = 'error';
+      state.message = `Cannot update hot dog: ${payload.type}`;
     },
     [removeHotDog.pending]: state => {
       state.status = 'deleting';
     },
     [removeHotDog.fulfilled]: (state, { meta }) => {
       state.status = 'success';
+      state.message = 'Hot dog deleted successfully';
       const { arg } = meta;
       hotDogAdapter.removeOne(state, arg);
     },
-    [removeHotDog.rejected]: state => {
+    [removeHotDog.rejected]: (state, { payload }) => {
       state.status = 'error';
+      state.message = `Cannot delete hot dog: ${payload.type}`;
     }
   }
 });
@@ -90,6 +100,9 @@ export const getModal = () => createDraftSafeSelector(getRootState, state => sta
 
 export const getStatus = () => createDraftSafeSelector(getRootState,
   state => state.hotDogs.status);
+
+export const getMessage = () => createDraftSafeSelector(getRootState,
+  state => state.hotDogs.message);
 
 export const {
   openCreateModal,
